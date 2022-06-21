0147b6f8275f5e669d3394bbce6fb757e44f3727
Kashif Siddique <61106834+mkashifsiddiqu@users.noreply.github.com>
2022-06-21 13:56:19 +0500

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,33 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import Header from '../components/Header'
+import Nav from '../components/Nav'
+import request from '../utlity/request'
+import Result from '../components/Result'
+export default function Home({results}) {
+  return (
+    <div>
+      <Head>
+        <title>HuluPk</title>
+        <meta name="description" content="website Is Develop by Kashif" />
+      </Head>
+      {/* header of Website  */}
+      <Header/>
+      <Nav/>
+      <Result results={results}/>
+    </div>
+  )
+}
+export async function getServerSideProps(context){
+  const genre = context.query.genre;
+  const reqt = await fetch(`https://api.themoviedb.org/3//trending/all/week?api_key=a521758a2308eeb39e03a4730afbd4bf&language=en-us`).then((res)=>res.json())
+  if(genre!=null)
+  { const reqt = await fetch(`https://api.themoviedb.org/3/${request[genre].url 
+    || request.fetchTrending.url}`).then((res)=>res.json())
+  }
+  return {
+    props:{
+      results:JSON.parse(JSON.stringify(reqt.results)),
+    }
+  }
+}
